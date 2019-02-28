ddf5c44ead7560754e6919e3d1bd33867eed8462
Camille Noufi <cnoufi@ccrma.stanford.edu>
2019-02-28 15:10:24 -0800

    mel extraction

--- Code/compute_melspectrogram.py
+++ Code/compute_melspectrogram.py
@@ -6,20 +6,25 @@ import argparse
 import random
 import os
 import librosa
+import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default='/usr/ccrma/media/projects/jordan/Datasets/DAMP-AG/audio/wav/train/local_balanced', help="Directory with the master valid DAMP-VocEx dataset")
+parser.add_argument('--data_dir', default='/Users/camillenoufi/Documents/datasets/VocEx-local/local_balanced/', help="Directory with the master valid DAMP-VocEx dataset")
 
 
 if __name__ == '__main__':
-   
+
+    # Process command line
    args = parser.parse_args()
-   
    assert os.path.isdir(args.data_dir), "Couldn't find the dataset at {}".format(args.data_dir)
-   
    master_path = args.data_dir
-   
-   # Get filenames in master directory and shuffle randomly
+
+   # Construct variables
+   wavX = '.wav'
+   meldict = {}
+
+   # Get filenames in dataset
    filenames = os.listdir(master_path)
-   
+   filepaths = [os.path.join(master_path, f) for f in filenames if f.endswith(wavX)]
+
    
