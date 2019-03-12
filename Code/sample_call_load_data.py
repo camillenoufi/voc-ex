a49fc58d9e09d5dc8cd8babc0dbc67fa4a9c43a9
Camille Noufi <cnoufi@ccrma.stanford.edu>
2019-03-11 21:12:07 -0700

    adjust sample call

--- Code/sample_call_load_data.py
+++ Code/sample_call_load_data.py
@@ -5,58 +5,57 @@ from load_data import DataLoader
 # Main TEST Routine
 if __name__ == '__main__':
 
-   #### CHANGE THIS PATH FOR YOUR LOCAL MACHINE'S COPY OF THE DATA FOLDER ###############
-   data_dir = '/Users/camillenoufi/Documents/datasets/VocEx-local/'
-   ####################
+    #### CHANGE THIS PATH FOR YOUR LOCAL MACHINE'S COPY OF THE DATA FOLDER ###############
+    data_dir = '/Users/camillenoufi/Documents/datasets/VocEx-local'
+    ####################
 
-   # Specify which partition you want (ie. local_balanced vs local_representative vs. development(validation) set)
-   partition = '/local_balanced/'
-   # Declare an instance of the data loader
-   dl = DataLoader(data_dir,partition)
-
-
-   """
-   Load list of files in the local training(or dev) set folder :
-   Returns list[str]
-   - len(list) is number of different audio files in the training(or dev) set
-   - str is a string of the specific /path/to/audiofile.wav
-   """
-   filepath_list = dl.load_filelist()
-   print('FileList Length:')
-   print(len(filepath_list))
+    # Specify which partition you want (ie. local_balanced vs local_representative vs. development(validation) set)
+    partition = 'local_dev/'
+    # Declare an instance of the data loader
+    dl = DataLoader(data_dir,partition)
 
 
+    """
+    Load list of files in the local training(or dev) set folder :
+    Returns list[str]
+    - len(list) is number of different audio files in the training(or dev) set
+    - str is a string of the specific /path/to/audiofile.wav
+    """
+    filepath_list = dl.load_filelist()
+    print('FileList Length:')
+    print(len(filepath_list))
 
-   """
-   Load Pre-computed log-mel-spectrogram features:
-   Call with Pickle (pkl) filename containing saved mel features
-   Returns dict{key:value}:
-   - key: (str) is a string of a specific /path/to/audiofile.wav in the local training(or dev) set folder
-   - value: (list[ 2D np.array ])
+
+
+    """
+    Load Pre-computed log-mel-spectrogram features:
+    Call with Pickle (pkl) filename containing saved mel features
+    Returns dict{key:value}:
+    - key: (str) is a string of a specific /path/to/audiofile.wav in the local training(or dev) set folder
+    - value: (list[ 2D np.array ])
             * len(list) = total number of feature arrays (inputs) created from one audio file
             * np.array = 2D slice of mel spectrogram (this is a single "input" into the neural network)
             * np.array.shape = (96 fbins, 172 time-frames)   **each time frame is ~10ms
    """
-   embed_dict = dl.load_embedding_dict(fname='dict_trainBal_feats.pkl')
-   print('Length of Feature Embeddings Dict:')
-   print(len(embed_dict))
+    embed_dict = dl.load_embedding_dict(fname='dict_dev_feats.pkl')
+    print('Length of Feature Embeddings Dict:')
+    print(len(embed_dict))
+    print(embed_dict[filepath_list[0]].size())
 
 
-   """
-   Loads dictionary of numeric country label "truths" for each specific audio file:
-   Call with .csv filename containing the metadata for all audio files in the set of interest
-   Returns dict{key:value}:
-   - key: (str) is a string of a specific /path/to/audiofile.wav in the local training(or dev) set folder
-   - value: (int) is the numeric country/accent label for this specific audio file
+    """
+    Loads dictionary of numeric country label "truths" for each specific audio file:
+    Call with .csv filename containing the metadata for all audio files in the set of interest
+    Returns dict{key:value}:
+    - key: (str) is a string of a specific /path/to/audiofile.wav in the local training(or dev) set folder
+    - value: (int) is the numeric country/accent label for this specific audio file
             * use this as the 'key' to acces the one-hot vector representation of the label
             * all feature inputs created from a specific audio file will use that audio file's numeric label
 
-   """
-   label_dict = dl.load_label_dict(metadata_file='local_train_bal.csv')
-   print('Length of embedding labels dict:')
-   print(len(label_dict))
-
-
+    """
+    label_dict = dl.load_label_dict(metadata_file='local_dev.csv')
+    print('Length of embedding labels dict:')
+    print(len(label_dict))
 
     """
     Load Dictionary of {numeric country labels:one-hot vector representations}:
